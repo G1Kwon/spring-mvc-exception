4fb9f058c83d208ac586eac1aa06bf2604369c33
g1.kwon <spring_shot@naver.com>
2021-09-05 16:57:42 +0900

    Servlet Exception - springboot errorPage_1

--- src/main/java/hello/exception/servlet/ServletExController.java
+++ src/main/java/hello/exception/servlet/ServletExController.java
@@ -15,6 +15,10 @@ public class ServletExController {
     public void errorEx() {
         throw new RuntimeException("예외 발생!"); }
 
+    @GetMapping("/error-400")
+    public void error400(HttpServletResponse response) throws IOException {
+        response.sendError(400, "400 오류!"); }
+
     @GetMapping("/error-404")
     public void error404(HttpServletResponse response) throws IOException {
         response.sendError(404, "404 오류!"); }
